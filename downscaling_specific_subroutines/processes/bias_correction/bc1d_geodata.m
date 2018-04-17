000d3f94a8208afa7319a7edf3312ae32a3f3823
Thomas Mosier <mosier.thomas@gmail.com>
2018-04-17 14:48:17 -0400

    Bug fixes to 1d bias correction
    Edited which input used to grab properties. This likely does not change performance within GCD package, but improves general usability.

--- downscaling_specific_subroutines/processes/bias_correction/bc1d_geodata.m
+++ downscaling_specific_subroutines/processes/bias_correction/bc1d_geodata.m
@@ -18,7 +18,7 @@
 % along with the Downscaling Package.  If not, see 
 % <http://www.gnu.org/licenses/>.
 
-function sSimPBc = bc1d_geodata(sSim2Bc, sSimH, sRefH, var, yrs, type)
+function sSimOut = bc1d_geodata(sSim2Bc, sSimH, sRefH, var, yrsBase, type)
 
 varLat = 'latitude';
 varLon = 'longitude';
@@ -45,77 +45,110 @@ varDate = 'date';
 %     end
 % end
 
-%Find lengths of data:
-nLat = length(sSim2Bc.(varLat));
-nLon = length(sSim2Bc.(varLon));
-
-
-%Initialize bias-corrected data structure array:
-sSimPBc = sSim2Bc;
-if isfield(sSimPBc,'val')
-    sSimPBc = rmfield(sSimPBc,'val');
-end
-if isfield(sSimPBc,'cdf')
-    sSimPBc = rmfield(sSimPBc,'cdf');
-end
-sSimPBc.(var) = nan(size(sSim2Bc.(var)),'single');
-
-
-%Find time-series elements to compare during bias correction:
-%For historic model:
-indSimH = find(sSimH.(varDate)(:,1) >= min(yrs) & sSimH.(varDate)(:,1) <= max(yrs));
-indRefH = find(sRefH.(varDate)(:,1) >= min(yrs) & sRefH.(varDate)(:,1) <= max(yrs));
-
-if numel(indRefH) ~= numel(indSimH)
-    [indSameSimH, ~] = ismember(sSimH.(varDate)(:,1), sRefH.(varDate)(:,1), 'rows');
-    [indSameRefH, ~] = ismember(sRefH.(varDate)(:,1), sSimH.(varDate)(:,1), 'rows');
+blStructSim2Bc = 0;
+if isstruct(sSim2Bc) && isstruct(sSimH)
+    blStructSim2Bc = 1;
     
-    indSimH = intersect(indSimH, sort(find(indSameSimH ~= 0)));
-    indRefH = intersect(indRefH, sort(find(indSameRefH ~= 0)));
-	warning('eQMgeodata:unequalYrs', ['The number of years present in the '...
-       'two datasets being compared are not equal. They are going to be set to the common years of ' num2str(min(sSimH.(varDate)(indSimH,1))) '-' num2str(max(sSimH.(varDate)(indSimH,1)))]); 
+    sSim2Bc = {sSim2Bc};
+    sSimH   = {sSimH};
+elseif ~iscell(sSim2Bc) || ~iscell(sSimH)
+    error('bc1dGeodata:classMismatch','The input data classes are not the same. This is now allowed.')
 end
 
+if iscell(sRefH) && numel(sRefH(:)) == 1
+    sRefH = sRefH{1};
+elseif ~isstruct(sRefH)
+    error('bc1dGeodata:classHistRef','Historic reference must be of class struct.')
+end
 
-%Do quantile mapping bias-correction
-for ii = 1 : nLat
-	for jj = 1 : nLon
-        %Find indices for each output grid cell:
-        [~, indLatSimH] = min(abs(sSim2Bc.(varLat)(ii) - sSimH.(varLat)));
-        [~, indLonSimH] = min(abs(sSim2Bc.(varLon)(jj) - sSimH.(varLon)));
-        
-        [~, indLatRefH] = min(abs(sSim2Bc.(varLat)(ii) - sRefH.(varLat)));
-        [~, indLonRefH] = min(abs(sSim2Bc.(varLon)(jj) - sRefH.(varLon)));
-        
-        if regexpbl(type, 'month')
-            sSimPBc.(var)(:,ii,jj) = bc1d_month(...
-                sRefH.(var)(indRefH,indLatRefH,indLonRefH), sRefH.(varDate)(indRefH,:), ...
-                sSimH.(var)(indSimH,indLatSimH,indLonSimH), sSimH.(varDate)(indSimH,:), ...
-                sSim2Bc.(var)(:,ii,jj), sSim2Bc.(varDate),...
-                type);
-        elseif regexpbl(type, {'day','window'})
-            %Find number of days (i.e. +/- current)
-            nDyWin = regexpi(type,'[\d]{1,3}day','match');
-%             nDyWin = regexp(type,'-?\d+\.?\d*|-?\d*\.?\d+','match');
-            if isempty(nDyWin) 
-                nDyWin = 15;
-            elseif iscell(nDyWin)
-                if numel(nDyWin) > 1
-                    error('bc1d:multDayWindows',[num2str(numel(nDyWin)) ' day window indicators were found.'])
-                else
-                    nDyWin = str2double(char(regexpi(nDyWin{1},'[\d]{1,3}','match')));
+if numel(sSim2Bc) ~= numel(sSimH)
+    error('bc1dGeodata:diffNumber', ['The simulation to bias correct has ' ...
+        num2str(numel(sSim2Bc)) ' members, but the historic simulation has ' ...
+        num2str(numel(sSimH)) ' members. This is not allowed.'])
+end
+
+%Initialize output
+sSimOut = sSim2Bc;
+
+%Loop over ensemble members
+for mm = 1 : numel(sSim2Bc)
+
+    %Find lengths of data:
+    nLat = length(sSim2Bc{mm}.(varLat));
+    nLon = length(sSim2Bc{mm}.(varLon));
+
+
+    %Initialize bias-corrected data structure array:
+    if isfield(sSimOut{mm},'val')
+        sSimOut{mm} = rmfield(sSimOut{mm},'val');
+    end
+    if isfield(sSimOut{mm},'cdf')
+        sSimOut{mm} = rmfield(sSimOut{mm},'cdf');
+    end
+    sSimOut{mm}.(var) = nan(size(sSim2Bc{mm}.(var)),'single');
+
+
+    %Find time-series elements to compare during bias correction:
+    %For historic model:
+    indSimH = find(sSimH{mm}.(varDate)(:,1) >= min(yrsBase) & sSimH{mm}.(varDate)(:,1) <= max(yrsBase));
+    indRefH = find(sRefH.(varDate)(:,1) >= min(yrsBase) & sRefH.(varDate)(:,1) <= max(yrsBase));
+
+    if numel(indRefH) ~= numel(indSimH)
+        [indSameSimH, ~] = ismember(sSimH{mm}.(varDate)(:,1), sRefH.(varDate)(:,1), 'rows');
+        [indSameRefH, ~] = ismember(sRefH.(varDate)(:,1), sSimH{mm}.(varDate)(:,1), 'rows');
+
+        indSimH = intersect(indSimH, sort(find(indSameSimH ~= 0)));
+        indRefH = intersect(indRefH, sort(find(indSameRefH ~= 0)));
+        warning('eQMgeodata:unequalYrs', ['The number of years present in the '...
+           'two datasets being compared are not equal. They are going to be set to the common years of ' num2str(min(sSimH{mm}.(varDate)(indSimH,1))) '-' num2str(max(sSimH{mm}.(varDate)(indSimH,1)))]); 
+    end
+
+
+    %Do quantile mapping bias-correction
+    for ii = 1 : nLat
+        for jj = 1 : nLon
+            %Find indices for each output grid cell:
+            [~, indLatSimH] = min(abs(sSim2Bc{mm}.(varLat)(ii) - sSimH{mm}.(varLat)));
+            [~, indLonSimH] = min(abs(sSim2Bc{mm}.(varLon)(jj) - sSimH{mm}.(varLon)));
+
+            [~, indLatRefH] = min(abs(sSim2Bc{mm}.(varLat)(ii) - sRefH.(varLat)));
+            [~, indLonRefH] = min(abs(sSim2Bc{mm}.(varLon)(jj) - sRefH.(varLon)));
+
+            if regexpbl(type, 'month')
+                sSimOut{mm}.(var)(:,ii,jj) = bc1d_month(...
+                    sRefH.(var)(indRefH,indLatRefH,indLonRefH), sRefH.(varDate)(indRefH,:), ...
+                    sSimH{mm}.(var)(indSimH,indLatSimH,indLonSimH), sSimH{mm}.(varDate)(indSimH,:), ...
+                    sSim2Bc{mm}.(var)(:,ii,jj), sSim2Bc{mm}.(varDate),...
+                    type);
+            elseif regexpbl(type, {'day','window'})
+                %Find number of days (i.e. +/- current)
+                nDyWin = regexpi(type,'[\d]{1,3}day','match');
+    %             nDyWin = regexp(type,'-?\d+\.?\d*|-?\d*\.?\d+','match');
+                if isempty(nDyWin) 
+                    nDyWin = 15;
+                elseif iscell(nDyWin)
+                    if numel(nDyWin) > 1
+                        error('bc1d:multDayWindows',[num2str(numel(nDyWin)) ' day window indicators were found.'])
+                    else
+                        nDyWin = str2double(char(regexpi(nDyWin{1},'[\d]{1,3}','match')));
+                    end
                 end
-            end
 
-            sSimPBc.(var)(:,ii,jj) = bc1d_day(...
-                sRefH.(var)(indRefH,indLatRefH,indLonRefH), sRefH.(varDate)(indRefH,:), ...
-                sSimH.(var)(indSimH,indLatSimH,indLonSimH), sSimH.(varDate)(indSimH,:), ...
-                sSim2Bc.(var)(:,ii,jj), sSim2Bc.(varDate),...
-                nDyWin, type);
-        else
-            error('eQmGeodata:unknownType',['eQM type ' type ' has not been programmed for.']);
+                sSimOut{mm}.(var)(:,ii,jj) = bc1d_day(...
+                    sRefH.(var)(indRefH,indLatRefH,indLonRefH), sRefH.(varDate)(indRefH,:), ...
+                    sSimH{mm}.(var)(indSimH,indLatSimH,indLonSimH), sSimH{mm}.(varDate)(indSimH,:), ...
+                    sSim2Bc{mm}.(var)(:,ii,jj), sSim2Bc{mm}.(varDate),...
+                    nDyWin, type);
+            else
+                error('eQmGeodata:unknownType',['eQM type ' type ' has not been programmed for.']);
+            end
         end
-	end
+    end
+end
+
+%Convert back to input format
+if blStructSim2Bc == 1
+    sSimOut = sSimOut{1};
 end
 
 %Various plots for testing:
